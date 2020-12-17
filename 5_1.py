1684ad88d0e68dd588e56ab5039570205a59f13a
AlexPereskokov <pereskokov_1997@mail.ru>
2020-12-17 16:39:42 +0500

    HW 6

--- 5_1.py
+++
@@ -1,8 +0,0 @@
-my_f = open("random_file.txt", "w")
-while True:
-    b = input("Enter something: ")
-    if b != '':
-        my_f.write(f'{b}\n')
-    else:
-        break
-my_f.close()
--- 5_2.py
+++
@@ -1,8 +0,0 @@
-my_f = open(r"C:\Users\User\Desktop\test_file.txt", "r", encoding='utf-8')
-count_str = 0
-for line in my_f:
-    count_str += 1
-    count_wrd = len(line.split())
-    print(f'Line number {count_str} contains {count_wrd} word(s)')
-print('Total number of lines', count_str)
-my_f.close()
